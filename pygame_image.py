a607a41bc958feed647787022e0965581c0b4a69
c0a22010 <c0a220106d@edu.teu.ac.jp>
2023-04-18 17:52:12 +0900

    演習問題

--- pygame_image.py
+++ pygame_image.py
@@ -6,11 +6,21 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ProjExD2023/ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ProjExD2023/ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img2 = pg.transform.rotozoom(kk_img, 10, 1.0)
-    
-    kk_list = [kk_img, kk_img2]
+    kk_list = []
+    """
+    ↓こうかとんのフレームごとの回転に関するリスト作成↓
+    """
+    for i in range(20):  
+        ls = []
+        if i < 11:
+            kk_imgrtzm = pg.transform.rotozoom(kk_img, i, 1.0)
+        else:
+            kk_imgrtzm = pg.transform.rotozoom(kk_img, 20-i, 1.0)
+        ls.append(kk_imgrtzm)
+        kk_list += ls * 5
 
     tmr = 0
 
@@ -19,10 +29,11 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_list[tmr%2], [300, 200])
+        screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(kk_list[tmr%100], [300, 200])
 
         pg.display.update()
         clock.tick(100)
